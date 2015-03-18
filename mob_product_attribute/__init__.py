3e21fbf1878c64dc979a6c85487174c9b350043a
Raf Ven <raf.ven@dynapps.be>
2015-03-18 11:56:14 +0100

    Implement Custom Attributes Extension

---
+++ mob_product_attribute/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#   OpenERP, Open Source Management Solution
+#    Copyright (C) 2013 webkul
+#	 Author :
+#				www.webkul.com	
+#
+##############################################################################
+import mob_product_attributes
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
