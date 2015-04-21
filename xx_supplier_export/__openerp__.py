c5d1357cbc84b1b4265606a6389b7292d234a459
Peter Langenberg <peter.langenberg@bubbles-it.be>
2015-04-21 10:33:00 +0200

    xx_supplier_export init

---
+++ xx_supplier_export/__openerp__.py
@@ -0,0 +1,16 @@
+{
+    "name": "extra templates for export data",
+    "version": "1.0",
+    "depends": [
+        "mail",
+    ],
+    "description": "Loads extra email templates",
+    "author": "DynApps",
+    "category": "DynApps/Customizations",
+    "website": "http://www.dynapps.be",
+    "data": [
+        "export_email.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
