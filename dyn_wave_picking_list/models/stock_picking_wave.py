c37450d539d597a8e33af6175dfa21c08c21f970
Juan Rial <jrial@safehex.be>
2015-10-17 01:15:45 +0200

    [ODW-85] Print Jim's report when printing all pickings in a wave

--- dyn_wave_picking_list/models/stock_picking_wave.py
+++ dyn_wave_picking_list/models/stock_picking_wave.py
@@ -81,6 +81,20 @@ class stock_picking_wave(models.Model):
             retval =  self.user_id.name
         return '%s.' % (retval, )
 
+    def print_picking(self, cr, uid, ids, context=None):
+        '''
+        This function print the report for all picking_ids associated to the picking wave
+        '''
+        context = dict(context or {})
+        picking_ids = []
+        for wave in self.browse(cr, uid, ids, context=context):
+            picking_ids += [picking.id for picking in wave.picking_ids]
+        if not picking_ids:
+            raise osv.except_osv(_('Error!'), _('Nothing to print.'))
+        context['active_ids'] = picking_ids
+        context['active_model'] = 'stock.picking'
+        return self.pool.get("report").get_action(cr, uid, [], 'xx_report_delivery_extended.report_delivery_master', context=context)
+
 
 class wave_location(models.Model):
     _name = 'wave_location'
