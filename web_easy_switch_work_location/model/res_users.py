61b5f53238d2bed2b65588dfb142a885a0cd2671
Stefan Rijnhart <stefan@opener.am>
2015-11-09 17:21:15 +0100

    [FIX] Allow users to write their own work location

--- web_easy_switch_work_location/model/res_users.py
+++ web_easy_switch_work_location/model/res_users.py
@@ -8,3 +8,9 @@ class res_users(Model):
     # Custom Function Section
     def change_current_work_location(self, cr, uid, work_location_id, context=None):
         return self.write(cr, uid, uid, {'work_location_id': work_location_id})
+
+    def _register_hook(self, cr):
+        res = super(res_users, self)._register_hook(cr)
+        self.pool['res.users'].SELF_WRITEABLE_FIELDS.append(
+            'work_location_id')
+        return res
