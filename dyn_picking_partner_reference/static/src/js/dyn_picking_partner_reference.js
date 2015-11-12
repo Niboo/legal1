e7654117eece5d781b7af44b4dbbe5563563db8c
Stefan Rijnhart <stefan@opener.am>
2015-11-12 14:09:58 +0100

    [ADD] Show a notification if a scan has no result

--- dyn_picking_partner_reference/static/src/js/dyn_picking_partner_reference.js
+++ dyn_picking_partner_reference/static/src/js/dyn_picking_partner_reference.js
@@ -19,6 +19,48 @@ openerp.dyn_picking_partner_reference = function(instance){
                 }
             }
             return res;
-        }
+        },
+
+        barcode_notify: function(msg) {
+            var el = this.$el.find('#barcode_notification');
+            el.value = msg;
+            el.show();
+            setTimeout(function() {el.hide()}, 5000);
+        },
+
+        scan: function(ean){ //scans a barcode, sends it to the server, then reload the ui
+            /* Overwrite this function from core (addons/stock/static/src/widgets.js).
+               We add in a notification if the product cannot be found */
+            var self = this;
+            var product_visible_ids = this.picking_editor.get_visible_ids();
+            return new instance.web.Model('stock.picking')
+                .call('process_barcode_from_ui', [self.picking.id, ean, product_visible_ids])
+                .then(function(result){
+                    if (result.filter_loc !== false){
+                        //check if we have receive a location as answer
+                        if (result.filter_loc !== undefined){
+                            var modal_loc_hidden = self.$('#js_LocationChooseModal').attr('aria-hidden');
+                            if (modal_loc_hidden === "false"){
+                                var line = self.$('#js_LocationChooseModal .js_loc_option[data-loc-id='+result.filter_loc_id+']').attr('selected','selected');
+                            }
+                            else{
+                                self.$('.oe_searchbox').val(result.filter_loc);
+                                self.on_searchbox(result.filter_loc);
+                            }
+                        }
+                    }
+                    if (result.operation_id !== false){
+                        self.refresh_ui(self.picking.id).then(function(){
+                            return self.picking_editor.blink(result.operation_id);
+                        });
+                    }
+                    /* Start of local change */
+                    else {
+                        this.barcode_notify(_t('No product was found for this supplier with code ') + ean);
+                    }
+                    /* End of local change */
+                });
+        },
+
     });
 }
