7a7137842e18ea69f6e86e2de12bb365cd3bfb7e
Stefan Rijnhart <stefan@opener.am>
2015-11-13 11:30:18 +0100

    [FIX] Use psycopg2 native substitution

--- xx_product_supplierinfo_tags/model/product_supplierinfo_tags.py
+++ xx_product_supplierinfo_tags/model/product_supplierinfo_tags.py
@@ -54,14 +54,16 @@ class product_product(models.Model):
         if 'process_barcode_from_ui_picking_id' in ctx:
             sp = self.pool.get('stock.picking').browse(cr,uid,[ctx.get('process_barcode_from_ui_picking_id')], context=context)[0]
             partner_ids = self.pool['res.partner'].search(
-                cr, uid, [('id', 'child_of', sp.partner_id.commercial_partner_id.id)])
+                cr, uid,
+                [('id', 'child_of', sp.partner_id.commercial_partner_id.id)]
+            ) or [0] # prevent passing empty list to Postgresql
             if sp.picking_type_id.code == 'incoming':
                 cr.execute("""
                 SELECT pp.id
                 FROM stock_picking sp, product_supplierinfo ps
                     INNER JOIN xx_product_supplierinfo_tags xpst ON xpst.res_id = ps.id
                                                                 AND xpst.res_model = 'product.supplierinfo'
-                                                                AND xpst.name like '%%%(needle)s%%'
+                                                                AND xpst.name like %(needle)s
                     INNER JOIN product_product pp ON pp.product_tmpl_id = ps.product_tmpl_id
                     INNER JOIN product_template pt ON pt.id = pp.product_tmpl_id
                 WHERE ps.name in %(partner_ids)s AND sp.id = %(p_id)s AND (pt.company_id IS NULL OR pt.company_id = sp.company_id)
@@ -71,26 +73,27 @@ class product_product(models.Model):
                     INNER JOIN product_product pp ON pp.product_tmpl_id = ps.product_tmpl_id
                     INNER JOIN product_template pt ON pt.id = pp.product_tmpl_id
                 WHERE ps.name in %(partner_ids)s AND sp.id = %(p_id)s  AND (pt.company_id IS NULL OR pt.company_id = sp.company_id)
-                    AND ps.product_code ilike '%%%(needle)s%%'
+                    AND ps.product_code ilike %(needle)s
                 UNION
                 SELECT pp.id
                 FROM stock_picking sp, product_supplierinfo ps
                     INNER JOIN product_product pp ON pp.product_tmpl_id = ps.product_tmpl_id
                     INNER JOIN product_template pt ON pt.id = pp.product_tmpl_id
                 WHERE ps.name in %(partner_ids)s AND sp.id = %(p_id)s  AND (pt.company_id IS NULL OR pt.company_id = sp.company_id)
-                    AND pt.manufacturer_pref ilike '%%%(needle)s%%'
+                    AND pt.manufacturer_pref ilike %(needle)s
                 UNION
                 SELECT pp.id
                 FROM stock_picking sp, product_supplierinfo ps
                     INNER JOIN product_product pp ON pp.product_tmpl_id = ps.product_tmpl_id
                     INNER JOIN product_template pt ON pt.id = pp.product_tmpl_id
                 WHERE ps.name in %(partner_ids)s AND sp.id = %(p_id)s  AND (pt.company_id IS NULL OR pt.company_id = sp.company_id) AND (
-                            pp.ean13 ilike '%%%(needle)s%%' OR
-                            pp.default_code ilike '%%%(needle)s%%'
+                            pp.ean13 ilike %(needle)s OR
+                            pp.default_code ilike %(needle)s
                         )
-                """ % {
+                """, {
                     'partner_ids': tuple(partner_ids),
-                    'needle':ctx.get('process_barcode_from_ui_barcode_str'), 'p_id':ctx.get('process_barcode_from_ui_picking_id')})
+                    'needle': '%%%s%%' % ctx['process_barcode_from_ui_barcode_str'],
+                    'p_id': ctx['process_barcode_from_ui_picking_id']})
                 query_result = cr.fetchall()
                 product_ids = ([x[0] for x in query_result])
             else:
@@ -101,10 +104,12 @@ class product_product(models.Model):
                     INNER JOIN product_product pp ON pp.product_tmpl_id = ps.product_tmpl_id
                     INNER JOIN product_template pt ON pt.id = pp.product_tmpl_id
                 WHERE sp.id = %(p_id)s  AND (pt.company_id IS NULL OR pt.company_id = sp.company_id) AND (
-                            pp.ean13 ilike '%%%(needle)s%%' OR
-                            pp.default_code ilike '%%%(needle)s%%'
+                            pp.ean13 ilike %(needle)s OR
+                            pp.default_code ilike %(needle)s
                         )
-                """ % {'needle':ctx.get('process_barcode_from_ui_barcode_str'), 'p_id':ctx.get('process_barcode_from_ui_picking_id')})
+                """, {
+                    'needle': '%%%s%%' % ctx['process_barcode_from_ui_barcode_str'],
+                    'p_id':ctx['process_barcode_from_ui_picking_id']})
                 query_result = cr.fetchall()
                 product_ids = [x[0] for x in query_result]
 
