e0d6e33c53d146ec546559f525689c8f3f957934
Stefan Rijnhart <stefan@opener.am>
2015-11-13 15:27:20 +0100

    [ADD] Tests and fixes

--- procurement_jit_no_sale/__openerp__.py
+++ procurement_jit_no_sale/__openerp__.py
@@ -1,10 +1,12 @@
 {
-    'name': 'Don\'t start sales procurements in time',
+    'name': 'Delay sales procurements when using just-in-time',
     'version': '1.0',
     'depends': [
         'procurement_jit',
         # Depend on purchase just to run full tests
         'purchase',
+        'sale_stock',
+        'procurement_jit_stock',
     ],
     'author': 'DynApps',
     'category': 'DynApps/Customizations',
--- procurement_jit_no_sale/models/procurement_order.py
+++ procurement_jit_no_sale/models/procurement_order.py
@@ -1,12 +1,16 @@
-from openerp import models
+# -*- coding: utf-8 -*-
+from openerp import models, api
 from openerp.addons.procurement_jit.procurement_jit import procurement_order
 
 
+@api.cr_uid
+@api.returns('self', lambda value: value.id)
 def create(self, cr, uid, vals, context=None):
     context = context or {}
     procurement_id = super(procurement_order, self).create(
         cr, uid, vals, context=context)
-    if not context.get('procurement_autorun_defer'):
+    if not vals.get('sale_line_id') and not context.get(
+            'procurement_autorun_defer'):
         self.run(cr, uid, [procurement_id], context=context)
         self.check(cr, uid, [procurement_id], context=context)
     return procurement_id
@@ -15,7 +19,19 @@ def create(self, cr, uid, vals, context=None):
 class ProcurementOrder(models.Model):
     _inherit = "procurement.order"
 
-    def _register_hook(self, cr):
-        res = super(ProcurementOrder, self)._register_hook(cr)
+    def __init__(self, pool, cr):
+        """ Monkeypatch our version of create() into the original model class..
+        This would normally happen in _register_hook, but as it turns out,
+        tests run before _register_hook is called """
+        res = super(ProcurementOrder, self).__init__(pool, cr)
         procurement_order.create = create
         return res
+
+    @api.multi
+    def run(self, autocommit=False):
+        """ Prevent the procurements to run after a sale order's move lines
+        are created """
+        if self.env.context.get('procurement_do_not_run'):
+            print "Not running"
+            return True
+        return super(ProcurementOrder, self).run(autocommit=autocommit)
