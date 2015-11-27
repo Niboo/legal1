bfd2a9db5bd7c107f97ddb9f0068976191608968
Juan Rial <jrial@safehex.be>
2015-11-27 15:12:54 +0100

    [IMP] Propose solution for pickings with unreserved move lines

--- dyn_wave_picking_list/models/stock_picking_wave.py
+++ dyn_wave_picking_list/models/stock_picking_wave.py
@@ -30,7 +30,7 @@ class stock_picking_wave(models.Model):
                     self.pool.get('stock.picking').write(cr, uid, picking.id, {'packages_assigned': True}, context=context)
                 for move_line in picking.move_lines:
                     if not move_line.reserved_quant_ids:
-                        raise osv.except_osv(_('Error!'), 'There is no reserved stock for product %s, in picking %s' % (move_line.product_id.name, picking.name))
+                        raise osv.except_osv(_('Error!'), 'There is no reserved stock for product %s, in picking %s - you can remove it from the wave then try again.' % (move_line.product_id.name, picking.name))
                     for quant in move_line.reserved_quant_ids:
                         if package_id and not quant.package_id:
                             self.pool.get('stock.quant').write(cr, SUPERUSER_ID, quant.id, {'package_id': package_id}, context=context)
