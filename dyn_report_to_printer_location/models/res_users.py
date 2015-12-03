2247f0ed5b22993fbc3ad6ad406d48bee8cc9c51
Stefan Rijnhart <stefan@opener.am>
2015-12-03 16:12:58 +0100

    [ADD] Reset flag on users; [RFR] Pep8

--- dyn_report_to_printer_location/models/res_users.py
+++ dyn_report_to_printer_location/models/res_users.py
@@ -12,15 +12,22 @@ class res_users(models.Model):
                 in _available_action_types(self)
                 if code != 'user_default']
 
-    printing_action = fields.Selection(_user_available_action_types_inherit)
-    work_location_id = fields.Many2one('work_location', string='Work Location', required=False)
+    printing_action = fields.Selection(
+        _user_available_action_types_inherit)
+    work_location_id = fields.Many2one(
+        'work_location', string='Work Location', required=False)
+    reset_work_location = fields.Boolean(
+        'Reset work location upon login', default=True)
 
     @api.multi
     def write(self, values):
         if values.get('work_location_id'):
-            self.search([('work_location_id', '=', values.get('work_location_id'))]).sudo().write({'work_location_id': False})
+            self.search(
+                [('work_location_id', '=', values.get('work_location_id'))]
+            ).sudo().write({'work_location_id': False})
         return super(res_users, self).write(values)
 
     _sql_constraints = [
-        ('work_location_id_uniq', 'unique(work_location_id)', 'Work Location must be unique!'),
+        ('work_location_id_uniq', 'unique(work_location_id)',
+         'Work Location must be unique!'),
     ]
