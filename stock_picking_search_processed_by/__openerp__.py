4b5da0ad9b9ca25a95a07af4ecdbfadc2dc62681
Stefan Rijnhart <stefan@opener.am>
2015-12-15 17:36:27 +0100

    [ADD] Search picking processed by user

---
+++ stock_picking_search_processed_by/__openerp__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 DynApps <http://www.dynapps.be>
+#
+#    @author Stefan Rijnhart <stefan.rijnhart@dynapps.be>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Search pickings by the user that last processed them',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'stock',
+    ],
+    'data': [
+        'views/stock_picking.xml',
+    ],
+    'author': 'DynApps',
+    'category': 'Warehouse Management',
+    'website': 'http://www.dynapps.be',
+    'installable': True,
+}
