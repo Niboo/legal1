7836c36ae559b31cca8b13105f6f5fdd24b825ce
Stefan Rijnhart <stefan@opener.am>
2015-12-22 12:46:42 +0100

    [FIX] Make barcode API accessible from new thread

--- dyn_report_to_printer_location/__openerp__.py
+++ dyn_report_to_printer_location/__openerp__.py
@@ -30,6 +30,10 @@ This can be used to e.g. select the nearest printer based on currently logged in
 location for any given report. E.g. a stock picking will be printed on the
 nearest A4 printer, while a shipping label will be printed on the nearest label
 printer.
+
+This module also includes a method to print asynchronously in a new thread.
+Note that this disables the authentication on the barcode generator API,
+because the session is not available in the new thread.
     """,
     'author': 'Dynapps',
     'depends': ['base_report_to_printer'],
---
+++ dyn_report_to_printer_location/controllers/main.py
@@ -0,0 +1,15 @@
+from openerp.addons.report.controllers.main import ReportController
+from openerp.http import route
+
+
+class NoAuthBarcode(ReportController):
+
+    @route(auth="none")
+    def report_barcode(
+            self, type, value, width=600, height=100, humanreadable=0):
+        """ Disable auth on the barcode generator API, so that we can call
+        it from the cheap threads that we create for printing asynchronously
+        """
+        return super(NoAuthBarcode, self).report_barcode(
+            type, value, width=width, height=height,
+            humanreadable=humanreadable)
