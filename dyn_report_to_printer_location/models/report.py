6c86b064e8abcf6942db4e91903e3e016b2947d8
Stefan Rijnhart <stefan@opener.am>
2015-12-28 09:22:10 +0100

    [FIX] Disable async reporting as this results in a fork bomb that exhausts the available database connections. To be refactored into a proper print queue using the connector framework for example.

--- dyn_report_to_printer_location/models/report.py
+++ dyn_report_to_printer_location/models/report.py
@@ -16,12 +16,20 @@ class Report(models.Model):
 
     @api.v7
     def print_document_async(self, cr, uid, ids, report, context=None):
+        return self.print_document(cr, uid, ids, report, context=context)
+        # Disable async reporting as this results in a fork bomb that exhausts
+        # the available database connections. To be refactored into a proper
+        # print queue using the connector framework for example.
         threading.Thread(
             target=self._print_document_async,
             args=(cr, uid, ids, report, context)).start()
 
     @api.v8
     def print_document_async(self, record, report):
+        return self.print_document(record, report)
+        # Disable async reporting as this results in a fork bomb that exhausts
+        # the available database connections. To be refactored into a proper
+        # print queue using the connector framework for example.
         threading.Thread(
             target=self.pool['report']._print_document_async,
             args=(self.env.cr, self.env.uid, record.ids, report,
