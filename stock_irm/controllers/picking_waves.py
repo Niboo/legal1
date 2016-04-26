f93864c1eadaa4e5e42bfdcdd0cbff484b729ec8
jerome guerriat <jerome@niboo.be>
2016-04-26 12:15:05 +0200

    ODW-10: create controller for wave creation

--- stock_irm/controllers/picking_waves.py
+++ stock_irm/controllers/picking_waves.py
@@ -39,22 +39,50 @@ class InboundController(http.Controller):
     @http.route('/picking_waves/create_picking', type='json', auth="user")
     def create_picking(self, **kw):
         env = http.request.env
-        env['stock.picking.wave'].create(
-            {'user_id': http.request.uid}
-        )
 
+        # create a wave
+        wave = env['stock.picking.wave'].create({
+            'user_id': http.request.uid,
+        })
+
+        # retrieve the picking type we want to search (stock -> client)
         picking_type_ids = env['stock.picking.type'].search([
             ('code', '=', 'outgoing'),
             ('active', '=', True)
-        ]).ids
-
-        pickings_ids = env['stock.picking'].search([
-            ('picking_type_id', 'in', picking_type_ids),
-            ('state', '=', 'waiting')
-        ], limit=15).ids
-
-        stock_move_ids = env['stock.move'].search([
-            ('pickinh_id', 'in', pickings_ids)
         ])
 
-        # stock_move_ids = sorted(line_list, key=itemgetter(0, 1, 2, 3))
+        # retrieve the pickings with that type
+        picking_ids = env['stock.picking'].search([
+            ('picking_type_id', 'in', picking_type_ids.ids),
+            ('state', '=', 'waiting')
+        ], limit=15)
+
+        # attach them to the wave and confirm the wave
+        wave.picking_ids = picking_ids
+        for picking in picking_ids:
+            picking.force_assign()
+        wave.confirm_picking()
+
+        # search the stock moves related to those pickings,
+        stock_move_ids = env['stock.move'].search([
+            ('picking_id', 'in', wave.picking_ids.ids)
+        ])
+
+        # sort the location by alphabetical name
+        picking_list = []
+        for move in sorted(stock_move_ids,
+                           key=lambda x: x.location_dest_id.name):
+            picking_list.append(
+                {'picking_id': move.picking_id.id,
+                 'product_id': move.product_id.id,
+                 'product_name': move.product_id.name,
+                 'product_image':
+                     "/web/binary/image?model=product.product&id=%s&field=image"
+                     % move.product_id.id,
+                 'location_id': move.location_id.id,
+                 'location_name': move.location_id.name
+                 })
+
+        results = {'status': 'ok', 'pickings': picking_list}
+        print results
+        return results
